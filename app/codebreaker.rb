69552b6131aa20707f50a61103b361781eac6407
Anna <anna.ruotanen@gmail.com>
2017-11-25 14:13:45 +0100

    Create code for tests A and B

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -9,15 +9,21 @@ class Codebreaker
     def start(secret_number)
       @secret_number = secret_number
 
-      output.puts "Welcome to Codebreaker"
+      output.puts "Welcome to Codebreaker " + secret_number
       output.puts "Enter guess:"
     end
 
     def guess(input)
       # Make sure to replace next line with the actual implemented marking algorithm,
       # using the @secret_number
-
-      output.puts "you typed '#{input}'"
+      if input.length > 4 || input.length < 4
+        output.puts "Try guessing a number with four digits"
+      elsif
+        input != @secret_number
+        output.puts ''
+      else input.include? "1" || input.include? "2" || input.include? "3" || input.include? "4"
+        output.puts "-"
+      end
     end
   end
 end
